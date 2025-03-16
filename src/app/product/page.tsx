90f9bddbdd0f616a3e7706fab99d5e63f938c78a
TonAlmeida <eltonsantos.almeida@outlook.com>
2025-03-16 11:10:47 -0300

    testing deploy

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -1,9 +1,9 @@
-"use client"
+'use client'
+
 import { BestSellers } from "@/components/BestSellers";
 import { Title } from "@/components/ui/Title";
 import { useSearchParams } from 'next/navigation'
-import { useEffect } from "react";
-import { Suspense } from "react";
+import { useEffect, Suspense } from "react";
 
 export default function Product() {
     const searchParams = useSearchParams();
@@ -15,21 +15,22 @@ export default function Product() {
 
     return (
         <main className="bg-white py-10 px-4">
-                <Suspense fallback={<div>Loading...</div>}>
-                    <Title title={name} />
-                    <div className="grid grid-cols-1 sm:grid-cols-3 py-4 border-2 rounded-lg">
-                        <div className="p-3 sm:col-span-2">
-                            <img className="h-72 m-auto" src={imageURL} alt={name} />
-                        </div>
-                        <div className="flex flex-col p-4 border-l-2">
-                            <p className="grow text-lg font-bold">{description}</p>
-                            <h2 className="text-3xl p-4">R$: {price}</h2>
-                            <button className="py-3 px-6 rounded-md w-full bg-blue-500 hover:bg-blue-400 text-white font-bold">Comprar agora</button>
-                            <button className="py-3 px-6 mb-7 mt-2 rounded-md w-full bg-gray-300 hover:bg-gray-200 text-risoflora font-bold">Adicionar ao carrinho</button>
-                        </div>
+            {/* Wrap the entire component in Suspense */}
+            <Suspense fallback={<div>Loading...</div>}>
+                <Title title={name} />
+                <div className="grid grid-cols-1 sm:grid-cols-3 py-4 border-2 rounded-lg">
+                    <div className="p-3 sm:col-span-2">
+                        <img className="h-72 m-auto" src={imageURL} alt={name} />
                     </div>
-                    <BestSellers />
-                </Suspense>
-            </main>
+                    <div className="flex flex-col p-4 border-l-2">
+                        <p className="grow text-lg font-bold">{description}</p>
+                        <h2 className="text-3xl p-4">R$: {price}</h2>
+                        <button className="py-3 px-6 rounded-md w-full bg-blue-500 hover:bg-blue-400 text-white font-bold">Comprar agora</button>
+                        <button className="py-3 px-6 mb-7 mt-2 rounded-md w-full bg-gray-300 hover:bg-gray-200 text-risoflora font-bold">Adicionar ao carrinho</button>
+                    </div>
+                </div>
+                <BestSellers />
+            </Suspense>
+        </main>
     )
 }
