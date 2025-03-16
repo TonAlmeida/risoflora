e0ac62b3f6da4bf3e7e0ba9437fe4db2a778d430
TonAlmeida <eltonsantos.almeida@outlook.com>
2025-03-16 16:05:05 -0300

    fixing to deploy

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -5,9 +5,9 @@ import { Title } from "@/components/ui/Title";
 import { useSearchParams } from 'next/navigation';
 import { Suspense, useEffect, useState } from "react";
 
-export default function Product() {
+// Create a separate component for the product details
+function ProductDetails() {
     const searchParams = useSearchParams();
-    
     const [name, description, category, price, imageURL] = searchParams.values();
 
     useEffect(() => {
@@ -17,20 +17,28 @@ export default function Product() {
     }, [name]);
 
     return (
+        <>
+            <Title title={name} />
+            <div className="grid grid-cols-1 sm:grid-cols-3 py-4 border-2 rounded-lg">
+                <div className="p-3 sm:col-span-2">
+                    <img className="h-72 m-auto" src={imageURL} alt={name} />
+                </div>
+                <div className="flex flex-col p-4 border-l-2">
+                    <p className="grow text-lg font-bold">{description}, {category}</p>
+                    <h2 className="text-3xl p-4">R$: {price}</h2>
+                    <button className="py-3 px-6 rounded-md w-full bg-blue-500 hover:bg-blue-400 text-white font-bold">Comprar agora</button>
+                    <button className="py-3 px-6 mb-7 mt-2 rounded-md w-full bg-gray-300 hover:bg-gray-200 text-risoflora font-bold">Adicionar ao carrinho</button>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default function Product() {
+    return (
         <main className="bg-white py-10 px-4">
             <Suspense fallback={<div>Loading...</div>}>
-                <Title title={name} />
-                <div className="grid grid-cols-1 sm:grid-cols-3 py-4 border-2 rounded-lg">
-                    <div className="p-3 sm:col-span-2">
-                        <img className="h-72 m-auto" src={imageURL} alt={name} />
-                    </div>
-                    <div className="flex flex-col p-4 border-l-2">
-                        <p className="grow text-lg font-bold">{description}, {category}</p>
-                        <h2 className="text-3xl p-4">R$: {price}</h2>
-                        <button className="py-3 px-6 rounded-md w-full bg-blue-500 hover:bg-blue-400 text-white font-bold">Comprar agora</button>
-                        <button className="py-3 px-6 mb-7 mt-2 rounded-md w-full bg-gray-300 hover:bg-gray-200 text-risoflora font-bold">Adicionar ao carrinho</button>
-                    </div>
-                </div>
+                <ProductDetails />
                 <BestSellers />
             </Suspense>
         </main>
