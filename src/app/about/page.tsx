58f30c95a2905cc2e7d00108a0b4a3dd80701cdf
TonAlmeida <eltonsantos.almeida@outlook.com>
2025-03-16 08:59:00 -0300

    fix problem with about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,18 +1,7 @@
-import { Banner } from "@/components/ui/Banner";
-import { OurClients } from "@/components/OurClients";
-import { Tabs } from "@/components/Tabs";
-import { OurWork } from "@/components/OurWork";
-import { OurTeam } from "@/components/OurTeam";
-
-export default function About() {
+export default function Page() {
     return (
-        <main className="h-full bg-white">
-            <Banner url="/about" title="Sobre" color="white" imageURL="" />
-            <Tabs />
-            <OurWork />
-            <OurTeam />
-            <hr className="bg-gray-500 w-4/5 mt-10 m-auto" />
-            <OurClients />
-        </main>
+        <div>
+            about
+        </div>
     )
-};
+}
